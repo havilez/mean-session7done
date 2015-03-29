af8e8f25f6304aa42f0eec82644c91b17031dcd4
havilez <havilez@gmail.com>
2015-03-29 08:25:59 -0400

    All issues fixed, except animate feature

---
+++ angular/app/app.js
@@ -0,0 +1,17 @@
+var portfolioApp = angular.module("portfolioSharingApp", ["ngRoute"]);
+
+portfolioApp.config(function($routeProvider){
+	$routeProvider
+	.when("/portfolios", {
+		controller: "PortfolioListController",
+		templateUrl: "app/partials/portfolio_list_partial.html"
+	})
+	.when("/portfolio/:portfolio_name", {
+		controller: "PortfolioViewController",
+		templateUrl: "app/partials/portfolio_viewer.html"
+	})
+	.when("/", { redirectTo: "/portfolios"
+	})
+	.otherwise({redirectTo: "/404_page"
+	});
+});
